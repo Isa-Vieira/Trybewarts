9872b2667174308e4f231d33749d4039630dc2b6
Isabelly Rocha Vieira <isabellyrvieira@icloud.com>
2022-05-05 19:51:03 -0300

    requisito-3-concluido

---
+++ script.js
@@ -0,0 +1,16 @@
+const botao1 = document.querySelector('.botao-entrar');
+
+function clicbotao() {
+  const botao2 = document.querySelector('.email');
+  const botao3 = document.querySelector('.senha');
+  console.log(botao2.value);
+  if (botao2.value === 'tryber@teste.com' && botao3.value === '123456') {
+    alert('Olá, Tryber!');
+  } else {
+    alert('Email ou senha inválidos.');
+  }
+}
+botao1.addEventListener('click', clicbotao);
+
+/* }; */
+/* clicbotao() */
